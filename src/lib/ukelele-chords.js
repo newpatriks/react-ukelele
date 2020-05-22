8aa72f70a2d01b6a4aa983333ecbed44a100c467
Jordi Llobet <newpatriks@gmail.com>
2020-05-22 17:13:14 -0400

    Updating dependencies. Update structure. Move to hooks

---
+++ src/lib/ukelele-chords.js
@@ -0,0 +1,289 @@
+export default {
+  A: [
+    {
+      string: [3],
+      fret: 1,
+      fingerId: 1
+    },
+    {
+      string: [4],
+      fret: 2,
+      fingerId: 2
+    }
+  ],
+  Am: [
+    {
+      string: [4],
+      fret: 2,
+      fingerId: 1
+    }
+  ],
+  A7: [
+    {
+      string: [3],
+      fret: 1,
+      fingerId: 1
+    }
+  ],
+  B: [
+    {
+      string: [1, 2],
+      fret: 2,
+      fingerId: 1
+    },
+    {
+      string: [3],
+      fret: 3,
+      fingerId: 2
+    },
+    {
+      string: [4],
+      fret: 4,
+      fingerId: 3
+    }
+  ],
+  Bm: [
+    {
+      string: [1, 2, 3],
+      fret: 2,
+      fingerId: 1
+    },
+    {
+      string: [4],
+      fret: 4,
+      fingerId: 3
+    }
+  ],
+  B7: [
+    {
+      string: [1, 2, 4],
+      fret: 2,
+      fingerId: 1
+    },
+    {
+      string: [3],
+      fret: 3,
+      fingerId: 2
+    }
+  ],
+  C: [
+    {
+      string: [1],
+      fret: 3,
+      fingerId: 1
+    }
+  ],
+  Cm: [
+    {
+      string: [1, 2, 3],
+      fret: 3,
+      fingerId: 1
+    }
+  ],
+  C7: [
+    {
+      string: [1],
+      fret: 1,
+      fingerId: 1
+    }
+  ],
+  D: [
+    {
+      string: [2],
+      fret: 2,
+      fingerId: 1
+    },
+    {
+      string: [3],
+      fret: 2,
+      fingerId: 2
+    },
+    {
+      string: [4],
+      fret: 2,
+      fingerId: 3
+    }
+  ],
+  Dm: [
+    {
+      string: [2],
+      fret: 1,
+      fingerId: 1
+    },
+    {
+      string: [4],
+      fret: 2,
+      fingerId: 2
+    },
+    {
+      string: [3],
+      fret: 2,
+      fingerId: 3
+    }
+  ],
+  D7: [
+    {
+      string: [2, 3, 4],
+      fret: 2,
+      fingerId: 1
+    },
+    {
+      string: [1],
+      fret: 3,
+      fingerId: 3
+    }
+  ],
+  E: [
+    {
+      string: [4],
+      fret: 1,
+      fingerId: 1
+    },
+    {
+      string: [1],
+      fret: 2,
+      fingerId: 2
+    },
+    {
+      string: [3],
+      fret: 4,
+      fingerId: 4
+    }
+  ],
+  Em: [
+    {
+      string: [1],
+      fret: 2,
+      fingerId: 1
+    },
+    {
+      string: [2],
+      fret: 3,
+      fingerId: 2
+    },
+    {
+      string: [3],
+      fret: 4,
+      fingerId: 4
+    }
+  ],
+  E7: [
+    {
+      string: [4],
+      fret: 1,
+      fingerId: 1
+    },
+    {
+      string: [3],
+      fret: 2,
+      fingerId: 2
+    },
+    {
+      string: [1],
+      fret: 2,
+      fingerId: 3
+    }
+  ],
+  F: [
+    {
+      string: [2],
+      fret: 1,
+      fingerId: 1
+    },
+    {
+      string: [4],
+      fret: 2,
+      fingerId: 2
+    }
+  ],
+  Fm: [
+    {
+      string: [2],
+      fret: 1,
+      fingerId: 1
+    },
+    {
+      string: [4],
+      fret: 1,
+      fingerId: 2
+    },
+    {
+      string: [1],
+      fret: 3,
+      fingerId: 4
+    }
+  ],
+  F7: [
+    {
+      string: [2],
+      fret: 1,
+      fingerId: 1
+    },
+    {
+      string: [4],
+      fret: 2,
+      fingerId: 2
+    },
+    {
+      string: [3],
+      fret: 3,
+      fingerId: 3
+    },
+    {
+      string: [1],
+      fret: 3,
+      fingerId: 4
+    }
+  ],
+  G: [
+    {
+      string: [1],
+      fret: 2,
+      fingerId: 1
+    },
+    {
+      string: [3],
+      fret: 2,
+      fingerId: 2
+    },
+    {
+      string: [2],
+      fret: 3,
+      fingerId: 3
+    }
+  ],
+  Gm: [
+    {
+      string: [1],
+      fret: 1,
+      fingerId: 1
+    },
+    {
+      string: [3],
+      fret: 2,
+      fingerId: 2
+    },
+    {
+      string: [2],
+      fret: 3,
+      fingerId: 3
+    }
+  ],
+  G7: [
+    {
+      string: [2],
+      fret: 1,
+      fingerId: 1
+    },
+    {
+      string: [3],
+      fret: 2,
+      fingerId: 2
+    },
+    {
+      string: [1],
+      fret: 2,
+      fingerId: 3
+    }
+  ]
+};
